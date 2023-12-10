d05d9f05133c6539511078725899e3730973d386
Sahil Suman <sahilsuman933@gmail.com>
2023-12-10 16:14:32 +0530

    minor bug.
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

--- week-1/class_assignment/timer.js
+++ week-1/class_assignment/timer.js
@@ -2,20 +2,10 @@ function timer() {
     const startTime = new Date();
 
     setTimeout(function sayHello() {
+        const endTime = new Date() - startTime;
         console.log("Hello Sahil!")
-<<<<<<< HEAD
-
-        const endTime = new Date() - startTime;
         console.log("End Time:" + endTime);
-    }, 3000);
-
-    
-=======
-        const endTime = new Date() - startTime;
-        console.log("End Time:" + endTime)
     }, 1000);
-
->>>>>>> 4b50f8a (completed week-2 assignment)
 }
 
 timer()
